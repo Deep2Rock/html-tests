cf61632f066f2f9bdf154915b3b260889da4efbb
unknown <diak@fonix.kag.hu>
2022-10-20 12:08:01 +0200

    jó

--- SneakersReleases/index.js
+++ SneakersReleases/index.js
@@ -1,18 +1,14 @@
-const menu = document.getElementById("menu");
-const menu_items = document.getElementById("menu-items");
+const menu = document.getElementById("reveal-menu");
+const menu_items = document.getElementById("menu");
 let number_clicks = 0;
 
+menu.addEventListener('click', function onClick() {
 
 if (number_clicks == 0) {
-    menu.addEventListener('click', function onClick() {
-        menu_items.style.display = 'inline';
-        number_clicks ++;
-        console.log(number_clicks)
-    })
-} else {
-    menu.addEventListener('click', function onClick() {
-        menu_items.style.display = 'none';
-        number_clicks == 0;
-        console.log(number_clicks)
-    })
+    menu_items.style.display = 'inline';
+    number_clicks += 1;
+} else if (number_clicks >= 1){
+    menu_items.style.display = 'none';
+    number_clicks = 0;
 }
+})
